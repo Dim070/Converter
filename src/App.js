cbcf250601d72b582375b75f8e12a5831bfb30c7
Dim070 <afer93@gmail.com>
2022-03-08 22:28:17 +0700

    незначительные изменения

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Currencies from "./components/Currencies/Currencies";
 import Convert from "./components/Convert/Convert";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Currencies/>
-      <Convert />
-    </div>
-
-  );
+    return (
+        <div className="App">
+            {/*<Routes>*/}
+            {/*    <Route path="/" element={<Currencies/>}/>*/}
+            {/*    <Route path="/convert" element={<Convert/>}/>*/}
+            {/*</Routes>*/}
+            <Currencies/>
+            <Convert/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Currencies/Currencies.js
+++ src/components/Currencies/Currencies.js
@@ -1,15 +1,64 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Form, Stack, Table} from "react-bootstrap";
+import Option from "../db";
+import Rate from "../db";
+
+const currencies = [
+    {"id": "USD", "name": "US Dollar"},
+    {"id": "EUR", "name": "Euro"},
+    {"id": "RUB", "name": "Russian Ruble"},
+    {"id": "JPY", "name": "Japanese Yen"},
+    {"id": "CHF", "name": "Swiss Franc"},
+    {"id": "CNY", "name": "Chinese Yuan"}
+]
+const rates = [
+    {
+        "id": "USD",
+        "rates": {
+            "EUR": 0.8812,
+            "RUB": 76.7962,
+            "JPY": 115.36,
+            "CHF": 0.9255,
+            "CNY": 6.3606
+        }
+    },
+    {
+        "id": "EUR",
+        "rates": {
+            "USD": 1.1343,
+            "RUB": 86.9245,
+            "JPY": 130.85,
+            "CHF": 1.0492,
+            "CNY": 7.2170
+        }
+    },
+    {
+        "id": "RUB",
+        "rates": {
+            "USD": 0.01308,
+            "EUR": 0.01151,
+            "JPY": 1.5072,
+            "CHF": 0.01208,
+            "CNY": 0.0831
+        }
+    }
+]
 
 function Currencies() {
+
+    console.log()
     return (
         <Container className='w-50'>
             <Stack className='d-flex flex-row justify-content-center align-items-center' gap={3}>
                 <Form.Label className="">Base currency:</Form.Label>
                 <Form.Select className="w-50">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
+                    {currencies.map(cur => (
+                        <Option
+                            key = {cur.id}
+                            id={cur.id}
+                            name={cur.name}
+                        />
+                    ))}
                 </Form.Select>
             </Stack>
 
@@ -21,6 +70,11 @@ function Currencies() {
                 </tr>
                 </thead>
                 <tbody>
+                {rates.map(rat => (
+                    <Rate
+                    id={rat.id}
+                    rates={rat.rates}/>
+                ))}
                 <tr>
                     <td>1</td>
                     <td>Mark</td>
